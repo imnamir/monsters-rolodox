e1d3c26a3ae3a75c377385ceada43c333b41b199
nameer.as <nameer.as@pitsolutions.com>
2020-10-13 16:56:53 +0530

    Updates

---
+++ precache-manifest.b238318da6988e40e7ac8722a84af32f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6234324252af3ae842c33ae610a2d139",
+    "url": "/monsters-rolodox/index.html"
+  },
+  {
+    "revision": "5820e12aa1b36af4dfaf",
+    "url": "/monsters-rolodox/static/css/main.3042e544.chunk.css"
+  },
+  {
+    "revision": "cf431214204838791a22",
+    "url": "/monsters-rolodox/static/js/2.4c8b29f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodox/static/js/2.4c8b29f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5820e12aa1b36af4dfaf",
+    "url": "/monsters-rolodox/static/js/main.fd7ca325.chunk.js"
+  },
+  {
+    "revision": "c03136030e60f3539fed",
+    "url": "/monsters-rolodox/static/js/runtime-main.9c1f7ad9.js"
+  }
+]);
